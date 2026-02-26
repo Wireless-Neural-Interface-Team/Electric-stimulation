e8bf2ed732181ad91837abe361e6bfcbfbca5c2f
hepierrey <pierre.he@inserm.fr>
2026-02-26 09:46:22 +0100

    Update run_wavegene_gui.py

--- electric_stimulation/run_wavegene_gui.py
+++ electric_stimulation/run_wavegene_gui.py
@@ -4,7 +4,7 @@ Launcher for WaveGene - NI-DAQmx pulse generator.
 """
 
 import sys
-from Electric_stimulation.wavegene_gui import main
+from electric_stimulation.wavegene_gui import main
 
 if __name__ == "__main__":
     sys.exit(main())
